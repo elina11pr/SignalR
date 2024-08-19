16a0a22b9062f91c592458c3364205c35dee3aec
elina11pr <igorivna2811@gmail.com>
2024-08-19 12:34:25 +0300

    first commit

---
+++ AppClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string serverUrl = "http://localhost:5000/";
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl(serverUrl)
+                .Build();
+
+            try
+            {
+                await connection.StartAsync();
+
+                Console.WriteLine("З'єднано");
+
+                while (true)
+                {
+                    Console.Write(" Введіть ім'я: ");
+                    string userName = Console.ReadLine();
+
+                    Console.Write(" Повідомлення: ");
+                    string message = Console.ReadLine();
+
+                    var data = new { UserName = userName, Message = message };
+                    string json = JsonSerializer.Serialize(data);
+
+                    await connection.InvokeAsync("SendMessage", json);
+
+                    Console.WriteLine("Ваше повідомлення відправлено");
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"Помилка: {err.Message}");
+            }
+        }
+    }
+}
---
+++ AppServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace AppServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:5000/");
+            listener.Start();
+            Console.WriteLine("Listening on port 5000...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+
+                {
+                    string requestBody = reader.ReadToEnd();
+                    Console.WriteLine($"Request body: {requestBody}");
+                }
+            }
+
+        }
+    }
+}
